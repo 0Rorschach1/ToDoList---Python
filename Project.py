ed828db1aef0c1c12b6eb6ba307f7f0d1ec93333
0Rorschach1 <158524039+0Rorschach1@users.noreply.github.com>
2025-10-19 20:06:36 +0330

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,8 +1,4 @@
-import os
-import uuid
-from datetime import datetime
-from typing import List, Optional, Tuple, Any
-from dotenv import load_dotenv
+
 
 from user import User
 from task import Task
